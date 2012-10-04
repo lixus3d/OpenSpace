07d7da9cc50a43bc3641e330340353d0738a3341
Lixus3d <developpement@adreamaline.com>
2012-10-04 14:33:42 +0200

    Adding vector maths functions

--- vector.js
+++ vector.js
@@ -9,8 +9,7 @@ OBJECTS.vector = function(x,y){
 	this.init = function(x,y){
 		if(x===undefined) x=0;
 		if(y===undefined) y=0;
-		vector.x = x;
-		vector.y = y;
+		vector.set(x,y);
 	};
 
 	this.randomize = function(maxX,maxY){
@@ -19,6 +18,46 @@ OBJECTS.vector = function(x,y){
 		vector.x = Math.random()*maxX*signX;
 		vector.y = Math.random()*maxY*signY;
 	};
+
+	this.set = function(x,y){
+		vector.x = x;
+		vector.y = y;
+	};
+
+	this.add = function(V){
+		vector.x += V.x;
+		vector.y += V.y;
+	};
+
+	this.sub = function(V){
+		vector.x -= V.x;
+		vector.y -= V.y;
+	};
+
+	this.mult = function(V){
+		vector.x *= V.x;
+		vector.y *= V.y;
+	};
+
+	this.dot = function(V){
+		var product = 0;
+		product += vector.x * V.x;
+		product += vector.y * V.y;
+		return product;
+	};
+
+	this.mag = function(){
+		return Math.sqrt( vector.dot(vector) );
+	};
+
+	this.normalize = function(){
+		var mag = vector.mag();
+		if(mag!==0){
+			vector.x = vector.x / mag;
+			vector.y = vector.y / mag;
+		}
+	};
+
 
 	this.init(x,y);
 };
