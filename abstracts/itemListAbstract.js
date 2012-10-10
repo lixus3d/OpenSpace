e7620537512674bb6847155e2051a4515df0e375
Lixus3d <developpement@adreamaline.com>
2012-10-10 16:58:36 +0200

    Completly changing the id generation and array storage

--- abstracts/itemListAbstract.js
+++ abstracts/itemListAbstract.js
@@ -12,7 +12,10 @@ ABSTRACTS.itemListAbstract = function(OpenSpaceObject){
     items.OpenSpace = null;
 
     items.list = [];  // list of item
-    items.idList = [];
+    items.list = {};
+    items.listLength = 0;
+
+    //items.idList = [];
 
 
     this.init = function(OpenSpaceObject){
@@ -25,17 +28,31 @@ ABSTRACTS.itemListAbstract = function(OpenSpaceObject){
      */
     this.addItem = function(item){
 
+        if(item.getId()===0){
+            uid = items.generateId();
+            item.setId(uid);
+        }
+        else uid = item.getId();
+
+        items.list[uid] = item;
+        items.listLength++;
+
 		// add item to the list
-		this.list.push(item);
+		//this.list.push(item);
 		// compute itemId
-		itemId = this.list.length-1;
+		//itemId = this.list.length-1;
 		// set the itemId
-        item.setId(itemId);
+        //item.setId(itemId);
         // push the id to the idList
-        this.idList.push(itemId);
+        //this.idList.push(itemId);
 
         item.activate();
     };
+
+    this.generateId = function(){
+        var uid = Math.random()*100+':'+Math.random()*100;
+        return uid;
+    }
 
     /**
      * Kill a item, update the list, update the current selection
@@ -44,11 +61,17 @@ ABSTRACTS.itemListAbstract = function(OpenSpaceObject){
     this.killItem = function(itemId){
 
         // get normal list position
+        if( items.list[itemId] !== undefined){
+            delete items.list[itemId];
+            items.listLength--;
+        }
+        /*
         var index = this.idList.indexOf(itemId);
         if(index!=-1){
             this.list.splice(index,1);
             this.idList.splice(index,1);
         }
+         */
     };
 
     /**
@@ -64,11 +87,16 @@ ABSTRACTS.itemListAbstract = function(OpenSpaceObject){
      */
     this.tick = function(){
         //log(items.list);
+        for( uid in items.list){
+            items.list[uid].tick();
+        }
+        /*
         $.each(items.list,function(k,item){
             if(item){
                 item.tick();
             }
         });
+         */
     };
 
     this.init(OpenSpaceObject);
