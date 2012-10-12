412731c611f8c9e25b346539371b038cad8e8db1
Lixus3d <developpement@adreamaline.com>
2012-10-12 09:54:23 +0200

    Automatically push player name & color @ start

--- start.js
+++ start.js
@@ -3,5 +3,7 @@ var OpenSpace = new OBJECTS.OpenSpace();
 
 window.setTimeout(function(){
 	OpenSpace.start();
+	$('input[name="playerName"]').trigger('change');
+	$('input[name="playerColor"]').trigger('change');
 },100);
 
