e33a854ae012a966134209fd4eff3b3f58da73fd
Lixus3d <developpement@adreamaline.com>
2012-10-12 09:54:34 +0200

    Drawing the scoreBoard

--- scoreBoard.js
+++ scoreBoard.js
@@ -9,11 +9,30 @@ OBJECTS.scoreBoard = function(OpenSpaceObject){
 	};
 
 	this.draw = function(){
-
+		var context = scoreBoard.OpenSpace.drawer.context;
+		context.fillStyle = '#555';
+		var y = 70;
+        context.fillText('Scores :',10,70);
+        var sortedPlayer = [];
+        for( uid in scoreBoard.OpenSpace.players.list ){
+			var player = scoreBoard.OpenSpace.players.list[uid];
+			var key = player.score;
+			while(sortedPlayer[key] !== undefined){
+				key -=1;
+			}
+			sortedPlayer[key] = player;
+        }
+        sortedPlayer.reverse();
+        for( order in sortedPlayer){
+        	y += 14;
+        	var player = sortedPlayer[order];
+			context.fillStyle = player.color;
+			context.fillText(player.name+' '+player.score+' ('+player.killed+')',10,y);
+        }
 	};
 
 	this.tick = function(){
-
+		scoreBoard.draw();
 	};
 
 	this.init(OpenSpaceObject);
